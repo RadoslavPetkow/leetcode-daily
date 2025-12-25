e46e249f55a7d8a284c44b9b562c38ea076d2db4
Radoslav Petkov <156192995+RadoslavPetkow@users.noreply.github.com>
2025-12-25 14:10:10 +0200

    Add files via upload

---
+++ medium/3075. Maximize Happiness of Selected Children/solution.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        happiness.sort(reverse=True)
+        ans = 0
+        for i in range(k):
+            ans += max(happiness[i] - i, 0)
+        return ans
