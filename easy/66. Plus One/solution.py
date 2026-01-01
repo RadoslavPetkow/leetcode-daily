0ed8af7f5416817d5db664ef08f7662b943f991b
Radoslav Petkov <156192995+RadoslavPetkow@users.noreply.github.com>
2026-01-01 15:05:26 +0200

    Add files via upload

---
+++ easy/66. Plus One/solution.py
@@ -0,0 +1,6 @@
+from typing import List
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        num = int(''.join(map(str, digits))) + 1
+        return list(map(int, str(num)))
